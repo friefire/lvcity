468e67937cb8ce1bdd0cf80732522594e2058a65
'huozhonghun' <'279180526@qq.com'>
2018-06-24 14:22:48 +0800

    新项目

---
+++ lvcitySS/src/com/neuedu/lvcity/common/FileUtils.java
@@ -0,0 +1,8 @@
+package com.neuedu.lvcity.common;
+
+
+public class FileUtils {
+
+	//Í¼Æ¬·þÎñÆ÷µØÖ·
+	public static String UPLOAD_PATH = "C:\\Program Files\\Apache Software Foundation\\apache-tomcat-8.0.30\\webapps\\uploads";
+}
